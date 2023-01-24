1b999b87b5ae23129883ea2aa159ae500a57a2ca
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-24 10:39:58 +0100

    List windows.

--- AppDelegate.cs
+++ AppDelegate.cs
@@ -1,10 +1,15 @@
+using ScreenCaptureKit;
+
 namespace screencapture_macos;
 
 [Register ("AppDelegate")]
 public class AppDelegate : NSApplicationDelegate {
-	public override void DidFinishLaunching (NSNotification notification)
+	public async override void DidFinishLaunching (NSNotification notification)
 	{
 		// Insert code here to initialize your application
+		var test = await SCShareableContent.GetShareableContentAsync(false, true);
+		foreach (var w in test.Windows)
+		    Console.WriteLine("Window: " + w.Title);
 	}
 
 	public override void WillTerminate (NSNotification notification)
